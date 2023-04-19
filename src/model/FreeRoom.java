f90070e79f5fc47b69b61af6014bc701ff0a7ab5
Anees Ahmad <inbox.a.ahmad@gmail.com>
2023-04-19 17:02:13 +0200

    commit

--- src/model/FreeRoom.java
+++ src/model/FreeRoom.java
@@ -1,10 +1,16 @@
 package model;
 
 public class FreeRoom extends Room {
+    private boolean isFree;
     public FreeRoom(String roomNumber, RoomType roomType) {
         super(roomNumber, 0.0, roomType); // Set the price to 0
     }
-
+    public boolean isFree() {
+        return isFree;
+    }
+    public void setFree(boolean free) {
+        isFree = free;
+    }
     @Override
     public String toString(){
         //return "FreeRoom " + getRoomNumber() + ", " + getRoomType() + ", " + "Price: " + getRoomPrice();
