6e1ca2990651d3c8d612499fdeaba868d0a4be7f
Anees Ahmad <inbox.a.ahmad@gmail.com>
2023-04-20 12:13:26 +0200

    added exception handling in CustomerService

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -3,17 +3,22 @@ package service;
 import model.Customer;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CustomerService{
-    private static final Map<String, Customer> customers = new HashMap<>();
+    private static final Map<String, Customer> customers = new ConcurrentHashMap<>();
     private static final CustomerService INSTANCE = new CustomerService();
     private CustomerService() {}
     public static CustomerService getInstance() {
         return INSTANCE;
     }
-    public static void addCustomer(String email, String firstName, String lastName) {
+    public static synchronized void addCustomer(String email, String firstName, String lastName) {
+        Objects.requireNonNull(email, "Email cannot be null");
+        Objects.requireNonNull(firstName, "First name cannot be null");
+        Objects.requireNonNull(lastName, "Last name cannot be null");
+
         Customer customer = new Customer(firstName, lastName, email);
         customers.put(email, customer);
     }
